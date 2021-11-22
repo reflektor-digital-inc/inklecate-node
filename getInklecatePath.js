7f4066f23430e8a612087ae5ac0a787d827abd9e
Stephane Paul <stephane@spaulconsultant.com>
2021-11-22 11:44:56 -0500

    chore: add linux support

--- getInklecatePath.js
+++ getInklecatePath.js
@@ -4,7 +4,7 @@ const {
 } = require('path');
 
 module.exports = () => (
-  process.platform === 'darwin' ?
+  process.platform === 'darwin' || process.platform === 'linux' ?
     join(getBinDir(), 'inklecate') :
     join(getBinDir(), 'inklecate.exe')
 );
